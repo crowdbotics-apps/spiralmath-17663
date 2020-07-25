81639c3cc79d524489064e4e20657ba3bba5f0c0
Natali Baranova <nbaranova@renderedsource.com>
2020-07-25 13:09:49 +0600

    Permission system - in progress.

--- home/api/v1/view/user.py
+++ home/api/v1/view/user.py
@@ -2,7 +2,7 @@ from rest_framework import exceptions, mixins, permissions, viewsets
 from rest_framework.permissions import AllowAny
 from rest_framework.generics import get_object_or_404
 from rest_framework.decorators import action
-from rest_framework.exceptions import ValidationError
+from rest_framework.exceptions import ValidationError, PermissionDenied
 from django.contrib.auth import password_validation
 from django.core.exceptions import ValidationError as DjangoValidationError
 from home.api.v1.serializer.auth import SignupSerializer
@@ -41,7 +41,20 @@ class UserViewSet(
 
     queryset = User.objects.all()
     serializer_class = UserSerializerBase
-    permission_classes = (permissions.AllowAny,)
+
+    def get_queryset(self: 'UserViewSet'):
+        """Custom filter permissions."""
+        queryset = super().get_queryset()
+        role = self.request.user.role
+        if role == User.ROLE.ADMIN:
+            # admin has access to all users
+            pass
+        elif role == User.ROLE.EDITOR:
+            # customer has access only to himself
+            queryset = queryset.filter(pk=self.request.user.pk)
+        else:
+            raise PermissionDenied
+        return queryset
 
     def get_serializer(self, *args, **kwargs):
         """
@@ -53,17 +66,48 @@ class UserViewSet(
             create                  UserCreate
         """
         if self.request.user.is_anonymous and AllowAny in self.permission_classes:
+            print("d1")
             kwargs['context'] = self.get_serializer_context()
             return self.serializer_class(*args, **kwargs)
-        if self.action == 'list':
-            serializer_class = UserList
-        elif self.action in {'update'}:  # TODO for Natali implement and add  'partial_update', 'retrieve'
-            serializer_class = UserUpdate
-        elif self.action == 'create':
-            serializer_class = UserCreate
+        invoker_role = self.request.user.role
+
+        # Use ADMIN fields by default in reset framework html forms
+        updating_role = User.ROLE.ADMIN
+        if args and isinstance(args[0], UserList.Meta.model):
+            updating_role = args[0].role
+        elif 'data' in kwargs and 'role' in kwargs['data']:
+            updating_role = kwargs['data']['role']
+        elif kwargs.get('instance'):
+            # occurs only when rendering html form by calling /user/1/ from browser (not ajax)
+            updating_role = kwargs['instance'].role
+        serializer_class = super().get_serializer_class()
+        if invoker_role == User.ROLE.ADMIN:
+            if self.action == 'list':
+                serializer_class = UserList
+            elif self.action == 'retrieve':
+                if updating_role in {User.ROLE.ADMIN, User.ROLE.EDITOR}:
+                    serializer_class = UserUpdate
+                else:
+                    serializer_class = None
+            elif self.action in {'update', 'partial_update'}:
+                if updating_role in {User.ROLE.ADMIN, User.ROLE.EDITOR}:
+                    serializer_class = None  # Admin edit form is the same for both roles  # TODO for Natali
+                else:
+                    serializer_class = None
+            elif self.action == 'create':
+                if updating_role in {User.ROLE.ADMIN, User.ROLE.EDITOR}:
+                    serializer_class = UserCreate  # Admin create form is the same for both roles
+                else:
+                    serializer_class = None
+        elif invoker_role == User.ROLE.EDITOR:
+            if self.action == 'retrieve':
+                serializer_class = None  # TODO for Natali
+            elif self.action in {'update', 'partial_update'}:
+                serializer_class = None  # TODO for Natali
+            elif self.action in {'list', 'create'}:
+                serializer_class = None  # TODO for Natali it's correct
         else:
             serializer_class = None
-
         if serializer_class is None:
             raise exceptions.ValidationError(detail={self.action: ['Permission error.']})
         kwargs['context'] = self.get_serializer_context()
--- home/api/v1/view/user_type.py
+++ home/api/v1/view/user_type.py
@@ -1,7 +1,11 @@
 from rest_framework import mixins, viewsets
+from django.contrib.auth import get_user_model
 
+from rest_framework.exceptions import PermissionDenied
 from home.api.v1.serializer.user_type import UserTypeSerializer
 from users.models import UserType
+
+User = get_user_model()
 
 
 class UserTypeViewSet(
@@ -16,3 +20,13 @@ class UserTypeViewSet(
 
     queryset = UserType.objects.all()
     serializer_class = UserTypeSerializer
+
+    def get_queryset(self: 'UserTypeViewSet'):
+        """Allows only ADMIN"""
+        queryset = super().get_queryset()
+        role = self.request.user.role
+        if role == User.ROLE.ADMIN:
+            pass
+        else:
+            raise PermissionDenied
+        return queryset
