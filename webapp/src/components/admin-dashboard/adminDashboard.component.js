e8806aef3ec843b3f0003f36e421a87c8370ab16
Raj <raj.parmar@crowdbotics.com>
2020-07-28 15:46:35 +0530

    dashboard page responsive

--- webapp/src/components/admin-dashboard/adminDashboard.component.js
+++ webapp/src/components/admin-dashboard/adminDashboard.component.js
@@ -12,28 +12,33 @@ const AdminDashboard = () => {
   ];
 
   const users = [
-    { action: "Authors", number: 28 },
-    { action: "Reviewers", number: 71 },
+    { action: "authors", number: 28 },
+    { action: "reviewers", number: 71 },
   ];
 
   return (
     <React.Fragment>
-      <h3 className="mt-3 text-muted">Questions</h3>
-      <Row className="mt-3">
-        {questions.map((el) => (
-          <Col key={el.action} md="3">
-            <DashboardBox action={el.action} number={el.number} />
-          </Col>
-        ))}
-      </Row>
-      <h3 className="mt-3  text-muted">Users</h3>
-      <Row className="mt-3">
-        {users.map((el) => (
-          <Col key={el.action} md="3">
-            <DashboardBox action={el.action} number={el.number} />
-          </Col>
-        ))}
-      </Row>
+      <h3 className="mt-5 text-muted dashboard-heading">Questions</h3>
+      <div className="item-list-view">
+        <Row className="mt-3">
+          {questions.map((el) => (
+            <Col key={el.action} md="3" className={`dashbaord-title ${el.action}`} >
+              <DashboardBox action={el.action} number={el.number} className={el.action}/>
+            </Col>
+          ))}
+        </Row>
+      </div>
+
+      <h3 className="mt-5  text-muted dashboard-heading">Users</h3>
+      <div className="item-list-view">
+        <Row className="mt-3">
+          {users.map((el) => (
+            <Col key={el.action} md="3" className={`dashbaord-title ${el.action}`}>
+              <DashboardBox action={el.action} number={el.number} />
+            </Col>
+          ))}
+        </Row>
+      </div>
     </React.Fragment>
   );
 };
