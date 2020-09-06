1f616458e9e367f8db689eceac09aaeb81701b0e
Raj <raj.parmar@crowdbotics.com>
2020-09-06 13:36:02 +0530

    unread count solved

--- frontend/src/helpers/utils.js
+++ frontend/src/helpers/utils.js
@@ -11,6 +11,12 @@ export const addUnreadCount = (userList, messageList) => {
    let unreadCount = 0;
    for (let i = 0; i < userList.length; i++) {
       for (let j = 0; j < messageList.length; j++) {
+         userList[i].unreadCount = 0;
+      }
+   }
+
+   for (let i = 0; i < userList.length; i++) {
+      for (let j = 0; j < messageList.length; j++) {
          if (messageList[j].users[0] === userList[i].id) {
             userList[i].unreadCount = messageList[j].unread_counter;
             unreadCount += messageList[j].unread_counter;
