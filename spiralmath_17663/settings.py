d49aef5c791b92037b0d95a86af6e941b5680103
Natali Baranova <nbaranova@renderedsource.com>
2020-10-08 21:41:40 +0600

    fixing aws settings

--- spiralmath_17663/settings.py
+++ spiralmath_17663/settings.py
@@ -220,6 +220,7 @@ AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
 AWS_STORAGE_REGION = os.getenv('AWS_STORAGE_REGION')
 AWS_DEFAULT_ACL = 'public-read'
+AWS_S3_SIGNATURE_VERSION = 's3v4'
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
