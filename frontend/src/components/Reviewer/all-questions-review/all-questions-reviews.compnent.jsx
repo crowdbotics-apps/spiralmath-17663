c66c6bcfc47487241430f7169f2df98b38dbcf79
raj <raj.parmar@crowdbotics.com>
2020-12-28 19:39:49 +0530

    issues done

--- frontend/src/components/Reviewer/all-questions-review/all-questions-reviews.compnent.jsx
+++ frontend/src/components/Reviewer/all-questions-review/all-questions-reviews.compnent.jsx
@@ -1,51 +1,50 @@
-import React, { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 //components
-import ReviewerQuestionsTable from "./../reviewer-questions-table/reviewer-questions-table";
-import Pagination from "./../../Common/pagination/pagination.component";
-import Layout from "../../ui/layout/layout.component";
+import ReviewerQuestionsTable from './../reviewer-questions-table/reviewer-questions-table';
+import Pagination from './../../Common/pagination/pagination.component';
+import Layout from '../../ui/layout/layout.component';
 
 // selectors
-import { selectAllQuestions } from "../../../redux/question/question.select";
-import { selectAllQuestionsCount } from "../../../redux/question/question.select";
+import { selectAllQuestions } from '../../../redux/question/question.select';
+import { selectAllQuestionsCount } from '../../../redux/question/question.select';
 
 //actions
-import questionActions from "../../../redux/question/question.action";
+import questionActions from '../../../redux/question/question.action';
 
 const AllQuestionsReviews = () => {
-   const dispatch = useDispatch();
-   //pagination
-   const [currentPage, setCurrentPage] = useState(1);
-   const [questionsPerPage] = useState(10);
+	const dispatch = useDispatch();
+	//pagination
+	const [currentPage, setCurrentPage] = useState(1);
+	const [questionsPerPage] = useState(10);
 
-   const questions = useSelector(selectAllQuestions);
-   const questionsCount = 7; //useSelector(selectAllQuestionsCount);
+	const questions = useSelector(selectAllQuestions);
+	const questionsCount = 7; //useSelector(selectAllQuestionsCount);
 
-   useEffect(() => {
-      dispatch(questionActions.getAllQuestions());
-   }, []);
+	useEffect(() => {
+		dispatch(questionActions.getAllQuestions());
+	}, []);
 
-   const indexOfLastQuestion = currentPage * questionsPerPage;
-   const indexOfFirstQuestion = indexOfLastQuestion - questionsPerPage;
-   const currentQuestions =
-      questions && questions.slice(indexOfFirstQuestion, indexOfLastQuestion);
+	const indexOfLastQuestion = currentPage * questionsPerPage;
+	const indexOfFirstQuestion = indexOfLastQuestion - questionsPerPage;
+	const currentQuestions = questions && questions.slice(indexOfFirstQuestion, indexOfLastQuestion);
 
-   const paginate = (pageNumber) => setCurrentPage(pageNumber);
+	const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
-   return (
-      <>
-         <Layout>
-            <ReviewerQuestionsTable questions={currentQuestions} all={true} />
-            <Pagination
-               usersPerPage={questionsPerPage}
-               totalUsers={questionsCount}
-               paginate={paginate}
-               currentPage={currentPage}
-            />
-         </Layout>
-      </>
-   );
+	return (
+		<>
+			<Layout>
+				<ReviewerQuestionsTable questions={currentQuestions} all={true} />
+				<Pagination
+					usersPerPage={questionsPerPage}
+					totalUsers={questionsCount}
+					paginate={paginate}
+					currentPage={currentPage}
+				/>
+			</Layout>
+		</>
+	);
 };
 
 export default AllQuestionsReviews;
