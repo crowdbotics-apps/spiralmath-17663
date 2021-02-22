f21200c4b82a6b46825971ebd04c21a37cb002bf
abel <abelradac@gmail.com>
2021-02-22 16:16:32 +0200

    update create questions on quizz

--- quiz_framework/api/v1/serializers.py
+++ quiz_framework/api/v1/serializers.py
@@ -10,11 +10,49 @@ class QuizQuestionsSerializer(serializers.ModelSerializer):
         fields = ['id', 'quiz', 'order', 'question']
 
 
+class QuizQuestionNestedSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = QuizQuestions
+        fields = ['id', 'question', 'order']
+        extra_kwargs = {
+            'order': {
+                'required': True
+            }
+        }
+
+
 class QuizFrameworksSerializer(serializers.ModelSerializer):
     author = serializers.HiddenField(default=serializers.CurrentUserDefault())
-    questions = QuizQuestionsSerializer(read_only=True, many=True)
+    questions = QuizQuestionNestedSerializer(many=True, required=False)
 
     class Meta:
         model = QuizFrameworks
         fields = ['id', 'author', 'grade', 'order', 'title', 'description',
                   'footer', 'sequence', 'questions']
+
+    def create(self, validated_data):
+        quiz_questions = validated_data.pop('questions')
+        quiz = super().create(validated_data)
+
+        question_objs = []
+        for question in quiz_questions:
+            question_objs.append(
+                QuizQuestions(
+                    quiz=quiz,
+                    order=question.get('order'),
+                    question=question.get('question')
+                )
+            )
+        QuizQuestions.objects.bulk_create(question_objs)
+        return quiz
+
+    def update(self, instance, validated_data):
+        quiz_questions = validated_data.pop('questions')
+        quiz_questions_pks = [_.id for _ in quiz_questions]
+
+        for question in QuizQuestions.objects.filter(pk__in=quiz_questions_pks):
+            pass
+
+        return super().update(instance, validated_data)
+
