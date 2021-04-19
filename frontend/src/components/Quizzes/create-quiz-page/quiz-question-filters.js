92e82498fef75d306f924b86c630fdbde410163b
Raj <rajparmar7879@gmail.com>
2021-04-19 17:40:25 +0530

    edit quiz state fix

--- frontend/src/components/Quizzes/create-quiz-page/quiz-question-filters.js
+++ frontend/src/components/Quizzes/create-quiz-page/quiz-question-filters.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useSelector } from "react-redux";
 import { Form, Col, Button } from "react-bootstrap";
 import styled from "styled-components";
