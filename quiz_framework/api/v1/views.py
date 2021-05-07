dc20918eb6b4f753482089310b4462b3318b7f62
abel <abelradac@gmail.com>
2021-05-07 15:49:29 +0300

    fix grade filter

--- quiz_framework/api/v1/views.py
+++ quiz_framework/api/v1/views.py
@@ -3,6 +3,7 @@ from rest_framework import mixins
 from rest_framework import permissions
 
 from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
 
 from home.api.v1.permissions import CreateQuestionPermission
 from quiz_framework.models import QuizFrameworks, QuizQuestions
@@ -12,7 +13,7 @@ from .serializers import QuizFrameworksSerializer, QuizQuestionsSerializer
 class QuizViewSet(viewsets.ModelViewSet):
     serializer_class = QuizFrameworksSerializer
     permission_classes = [permissions.IsAuthenticated, CreateQuestionPermission]
-    filter_backends = [filters.OrderingFilter]
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
     filterset_fields = ['grade']
     ordering_fields = ['sequence', 'title']
     queryset = QuizFrameworks.objects.all()
