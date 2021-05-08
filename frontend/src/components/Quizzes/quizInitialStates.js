7b46c79b296c0bd82ca69139d0187f067c902a6c
Raj <rajparmar7879@gmail.com>
2021-05-08 11:05:25 +0530

    persist filters after route change

---
+++ frontend/src/components/Quizzes/quizInitialStates.js
@@ -0,0 +1,11 @@
+export const filterInitialState = {
+  grade_level: "_Any",
+  mills_difficulty_level: "_Any",
+  dok: "_Any",
+  question_style: "_Any",
+  summative_status: "_Any",
+  state_model: "_Any",
+  standard_code: "_Any",
+  content_source: "",
+  author_memo: "",
+};
